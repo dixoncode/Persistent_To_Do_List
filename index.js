3a0ca1770517418ebe9289b6053ba52bc3e21d98
dixoncode <dicksonabuka@outlook.com>
2024-09-01 16:05:23 +0300

    Adding the ability to render items

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // index.js
+let items = ["1","2"];
+
+const itemsDiv = document.getElementById("items")
+
+function renderItems() {
+    itemsDiv.innerHTML = null;
+
+    for (const [idx, item] of Object.entries(items)) {
+        const text = document.createElement("p")
+        text.textContent = item;
+
+        itemsDiv.appendChild(text)
+    }
+}
+
+function loadItems() {}
+
+function saveItems() {}
+
+function addItem() {}
+
+function removeItem() {}
